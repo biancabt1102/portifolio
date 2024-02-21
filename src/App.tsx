f6d3c5f174e5a6001d21c2f52875fb415e349586
Bianca Teixeira <biancabt1102@gmail.com>
2024-02-21 13:52:50 -0300

    feat: Add routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,24 @@
 import Home from "./pages/Home";
+import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider } from "react-router-dom";
+import Projetos from "./pages/Projetos";
+import Contato from "./pages/Contato";
+import SobreMim from "./pages/SobreMim";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<Home />}>
+      <Route path="sobremim" element={<SobreMim />} />
+      <Route path="projetos" element={<Projetos/>} />
+      <Route path="contato" element={<Contato />}/>
+    </Route>
+  )
+);
 
 export default function App() {
+
   return (
-    <div className="">
-      <Home />
+    <div>
+      <RouterProvider router={router} />
     </div>
   );
 }
